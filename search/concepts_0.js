2e4294f5ec50e748f2b54c954ba34ffd28556d62
LineGM <LineGM@users.noreply.github.com>
2025-03-24 10:34:39 +0000

    deploy: 08fd1fd4564eaecff2f2a23fa892ea43300a4373

--- search/concepts_0.js
+++ search/concepts_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['ini_3a_3astreamextractable_0',['StreamExtractable',['../de/dec/conceptini_1_1StreamExtractable.html',1,'ini']]]
+  ['ini_3a_3aini_5fmanager_3a_3astreamextractable_0',['StreamExtractable',['../d0/d4e/conceptini_1_1ini__manager_1_1StreamExtractable.html',1,'']]],
+  ['ini_3a_3astreamextractable_1',['StreamExtractable',['../de/dec/conceptini_1_1StreamExtractable.html',1,'ini']]]
 ];
