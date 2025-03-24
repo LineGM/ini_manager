b94b67387315462973234f3f9a4396bb55e4baa9
Gleb Gudkov <LineGM@yandex.ru>
2025-03-24 14:28:30 +0300

    chore: update docs-ci.cmake

--- cmake/docs-ci.cmake
+++ cmake/docs-ci.cmake
@@ -1,5 +1,52 @@
 cmake_minimum_required(VERSION 3.21)
 
+# ---- Process project() call in CMakeLists.txt ----
+
+file(READ "${src}/CMakeLists.txt" content)
+
+string(FIND "${content}" "project(" index)
+if(index EQUAL "-1")
+  message(FATAL_ERROR "Could not find \"project(\"")
+endif()
+string(SUBSTRING "${content}" "${index}" -1 content)
+
+string(FIND "${content}" "\n)\n" index)
+if(index EQUAL "-1")
+  message(FATAL_ERROR "Could not find \"\\n)\\n\"")
+endif()
+string(SUBSTRING "${content}" 0 "${index}" content)
+
+file(WRITE "${bin}/docs-ci.project.cmake" "docs_${content}\n)\n")
+
+macro(list_pop_front list out)
+  list(GET "${list}" 0 "${out}")
+  list(REMOVE_AT "${list}" 0)
+endmacro()
+
+function(docs_project name)
+  cmake_parse_arguments(PARSE_ARGV 1 "" "" "VERSION;DESCRIPTION;HOMEPAGE_URL" LANGUAGES)
+  set(PROJECT_NAME "${name}" PARENT_SCOPE)
+  if(DEFINED _VERSION)
+    set(PROJECT_VERSION "${_VERSION}" PARENT_SCOPE)
+    string(REGEX MATCH "^[0-9]+(\\.[0-9]+)*" versions "${_VERSION}")
+    string(REPLACE . ";" versions "${versions}")
+    set(suffixes MAJOR MINOR PATCH TWEAK)
+    while(NOT versions STREQUAL "" AND NOT suffixes STREQUAL "")
+      list_pop_front(versions version)
+      list_pop_front(suffixes suffix)
+      set("PROJECT_VERSION_${suffix}" "${version}" PARENT_SCOPE)
+    endwhile()
+  endif()
+  if(DEFINED _DESCRIPTION)
+    set(PROJECT_DESCRIPTION "${_DESCRIPTION}" PARENT_SCOPE)
+  endif()
+  if(DEFINED _HOMEPAGE_URL)
+    set(PROJECT_HOMEPAGE_URL "${_HOMEPAGE_URL}" PARENT_SCOPE)
+  endif()
+endfunction()
+
+include("${bin}/docs-ci.project.cmake")
+
 # ---- Generate docs ----
 
 set(DOXYGEN_IN ${PROJECT_SOURCE_DIR}/docs/Doxyfile.in)
